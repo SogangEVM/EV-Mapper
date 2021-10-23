68879e7da61eba7f3ef6430a97d9970fffcd7f95
jted0537 <jted0537@gmail.com>
2021-10-24 00:37:15 +0900

    Fix some iOS bugs

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+//import TMapSDK
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,22 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let tmapChannel = FlutterMethodChannel(name: "samples.flutter.dev/tmapInvoke",
+                                                    binaryMessenger: controller.binaryMessenger)
+      tmapChannel.setMethodCallHandler({
+          (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      })
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+//private func invokeTmap(result: FlutterResult) {
+//    var mapView = TMapView(frame: frame);
+//    self.view.addSubview(mapView);
+//    mapView.setAppKey("l7xxb841ff64eae6428a8b2ee688cd8abb94");
+//    TMapApi.invokeRoute("신도림역", coordinate:mapView.getCenter());
+//}
